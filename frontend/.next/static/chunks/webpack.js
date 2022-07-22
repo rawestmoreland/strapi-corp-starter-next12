5a3e4b7ea19613eb06cb598a3cbe01cd9927dc53
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-07-22 18:47:21 -0500

    image stuff

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0bd9791aef28478"; }
+/******/ 		__webpack_require__.h = function() { return "6b840d67d6da82cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/components/elements/image.js
+++ frontend/components/elements/image.js
@@ -1,39 +1,39 @@
-import { getStrapiMedia } from "utils/media"
-import Image from "next/image"
-import PropTypes from "prop-types"
-import { mediaPropTypes } from "utils/types"
+import { getStrapiMedia } from 'utils/media';
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+import { mediaPropTypes } from 'utils/types';
 
 const NextImage = ({ media, ...props }) => {
-  const { url, alternativeText, width, height } = media.data.attributes
+  const { url, alternativeText, width, height } = media.data.attributes;
 
   const loader = ({ src, width }) => {
-    return getStrapiMedia(src)
-  }
+    return getStrapiMedia(src, width);
+  };
 
   // The image has a fixed width and height
   if (props.width && props.height) {
     return (
-      <Image loader={loader} src={url} alt={alternativeText || ""} {...props} />
-    )
+      <Image unoptimized src={url} alt={alternativeText || ''} {...props} />
+    );
   }
 
   // The image is responsive
   return (
     <Image
       loader={loader}
-      layout="responsive"
-      width={width || "100%"}
-      height={height || "100%"}
-      objectFit="contain"
+      layout='responsive'
+      width={props.width || width || '100%'}
+      height={props.height || height || '100%'}
+      objectFit={props.cover ? 'cover' : 'contain'}
       src={url}
-      alt={alternativeText || ""}
+      alt={alternativeText || ''}
     />
-  )
-}
+  );
+};
 
 Image.propTypes = {
-  media: mediaPropTypes.isRequired,
+  media: mediaPropTypes,
   className: PropTypes.string,
-}
+};
 
-export default NextImage
+export default NextImage;
