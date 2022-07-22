0af862554ff2f0924879e8e2cf7e0b4622b88516
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-07-22 17:05:39 -0500

    get it working with new stuff

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "postcss-flexbugs-fixes",
-    "postcss-nesting",
-    "postcss-custom-properties",
-    "autoprefixer",
+    'postcss-import',
+    'tailwindcss/nesting',
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    'postcss-custom-properties',
+    'autoprefixer',
   ],
-}
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,7 @@
-const { colors } = require(`tailwindcss/defaultTheme`)
+const colors = require(`tailwindcss/colors`);
 
 module.exports = {
-  mode: "jit", // see https://tailwindcss.com/docs/just-in-time-mode
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
-  darkMode: false, // or "media" or "class"
+  content: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     extend: {
       colors: {
@@ -12,20 +10,20 @@ module.exports = {
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          md: "2rem",
+          DEFAULT: '1rem',
+          md: '2rem',
         },
       },
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
